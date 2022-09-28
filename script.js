98d4425414e035fe82346dd909d34f79110f17ca
keefeharris <keefeharrisjr@gmail.com>
2022-09-28 16:16:43 -0400

    Examples of the spread operator

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+  };
+
+// Spread Operator
+const arr = [1, 2, 3];
+const arr2 = [...arr, 4, 5, 6];
+console.log(arr2);
+console.log(...arr2);
+
+const mainMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(mainMenu);
